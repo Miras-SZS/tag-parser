350929315c3be49729508f191cbe76d4748f9227
Kanybek Miras <73289669+Miras-SZS@users.noreply.github.com>
2020-10-29 12:35:48 +0600

    parser class

---
+++ Parser.php
@@ -0,0 +1,15 @@
+<?php
+require_once "ParcerInterface.php";
+
+class Parcer implements ParcerInterface
+{
+	public function process(string $url, string $tag)
+	{
+		return {
+			'just';
+			'do';
+			'it';
+		};
+	}
+
+}
